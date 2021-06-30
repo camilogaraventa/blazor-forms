8ac8f8b9272cf2737a2fecfa3ae50e187f2fa4bc
Camilo Garaventa <camilo.garaventa@santexgroup.com>
2021-06-30 10:26:43 -0300

    Initial solution Example 1

---
+++ src/BlazorForms/Pages/Example1/Model.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BlazorForms.Pages.Example1
+{
+    public class Model
+    {
+        public String Name { get; set; }
+    }
+}
---
+++ src/BlazorForms/Pages/Example1/SimpleForm.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorForms.Pages.Example1
+{
+    public partial class SimpleForm
+    {
+        private Model _model;
+        private List<String> _messages;
+
+        protected override void OnInitialized()
+        {
+            _model = new Model();
+            _messages = new List<string>();
+        }
+
+        private void FormSubmit(EditContext editContext)
+        {
+            _messages.Add($"{DateTime.Now} form submitted. Valid: {editContext.Validate()}");
+        }
+    }
+}
