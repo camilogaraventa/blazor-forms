43f1858f30f7dd122acc0ab56f1e8c9270a3de84
Camilo Garaventa <camilo.garaventa@santexgroup.com>
2021-07-01 08:32:29 -0300

    Example 3 with data annotations validator included

---
+++ src/BlazorForms/Pages/Example3/FormWithDataAnnotationsValidator.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace BlazorForms.Pages.Example3
+{
+    public partial class FormWithDataAnnotationsValidator
+    {
+        private Model _model;
+        private List<String> _messages;
+
+        protected override void OnInitialized()
+        {
+            _model = new Model();
+            _messages = new List<String>();
+        }
+
+        private async Task OnValidFormSubmit()
+        {
+            await Task.Delay(1000);
+            _messages.Add($"{DateTime.Now} valid form submitted.{Environment.NewLine}{JsonSerializer.Serialize(_model, new JsonSerializerOptions() { WriteIndented = true })}");
+        }
+
+        private void OnInvalidFormSubmit(EditContext editContext)
+        {
+            _messages.Add($"{DateTime.Now} invalid form submitted.{Environment.NewLine}{String.Join(Environment.NewLine,editContext.GetValidationMessages())}");
+        }
+    }
+}
---
+++ src/BlazorForms/Pages/Example3/Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorForms.Pages.Example3
+{
+    public class Model
+    {
+        [Range(18, 80, ErrorMessage = "Age must be between 18 and 80.")]
+        public int Age { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        public string Name { get; set; }
+    }
+}
