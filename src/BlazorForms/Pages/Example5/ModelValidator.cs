73975e60dd5965bff832c41f201bea96fc75aeef
Camilo Garaventa <camilo.garaventa@santexgroup.com>
2021-07-01 10:02:20 -0300

    Added example with telerik inputs

---
+++ src/BlazorForms/Pages/Example5/ModelValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace BlazorForms.Pages.Example5
+{
+    public class ModelValidator : AbstractValidator<Model>
+    {
+        public ModelValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().MaximumLength(50);
+            RuleFor(x => x.Age).InclusiveBetween(18, 80);
+            RuleFor(x => x.Nested).SetValidator(new NestedModelValidator());
+        }
+    }
+
+    public class NestedModelValidator : AbstractValidator<NestedModel>
+    {
+        public NestedModelValidator()
+        {
+            RuleFor(x => x.Address).NotEmpty().MaximumLength(50);
+        }
+    }
+}
